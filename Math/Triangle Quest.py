eed436ef23a97921793ce6ca13603af459c476a8
Harikrishnan <harik.2010@gmail.com>
2018-06-25 16:24:45 +0200

    Alternate code added

--- Math/Triangle Quest.py
+++ Math/Triangle Quest.py
@@ -1,6 +1,8 @@
 
 for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
     print(int('1'*i)*i) 
+    
+    print((10**i//9)*i) #without using the string literal.
 
 
 #print(int('1'*i)*i) alternate solution but invalid as it uses string literl 
