59864f4025c5f93a5010e6263427f2a176f99092
Harikrishnan <harik.2010@gmail.com>
2018-07-03 12:12:50 +0200

    Alterantive code added

--- Collections/Collections_deque.py
+++ Collections/Collections_deque.py
@@ -10,3 +10,15 @@ for i in range(no_of_cmds):
     else:
         getattr(d,cmd)()
 print(*d,sep=' ')
+
+#----------------------Alterantive------------------------------------------#
+
+no_of_cmds = int(input())
+from collections import deque
+d = deque()
+
+for i in range(no_of_cmds):
+    cmd,*inp = input().split()
+    #print(cmd,*inp)
+    getattr(d,cmd)(*inp)    #* is 0 or more
+print(*d,sep=' ')
