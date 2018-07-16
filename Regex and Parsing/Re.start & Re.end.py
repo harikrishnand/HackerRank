834ff08f5bfe9bdd47aa09f13167faa9b4644561
Harikrishnan <harik.2010@gmail.com>
2018-07-16 14:08:35 +0200

    overalpping search,Substring search

---
+++ Regex and Parsing/Re.start & Re.end.py
@@ -0,0 +1,18 @@
+import re
+inp = input()
+mat = input()
+
+m = re.compile(r""+ re.escape(mat) +r"")
+st = 0
+for i in  range(len(inp)):
+    reobj =  m.search(inp,st)
+    if reobj:
+        print((reobj.start(),reobj.end()-1))
+        if len(mat) == 1:
+            st = reobj.end()
+        else:
+            st = reobj.end()-1
+    else:
+        break
+if not st:
+    print('(-1, -1)')
